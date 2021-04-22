fce6ce7d69358d19209919423e7999c59bb1b68e
Samaelish <semnd007@gmail.com>
2021-04-22 17:13:43 +0300

    done

---
+++ modules/helpers_array2.js
@@ -0,0 +1,21 @@
+export const helpers = {
+    getValue: function(array, key) {
+        let res = []
+        for (let i = 0; i < array.length; i++) {
+            res.push(array[i][key])
+        }
+        return res;
+    },
+    mapArray: function(array, key) {
+        let result = {}
+        for (let i = 0; i < array.length; i++) {
+            result[array[i][key]] = array[i]
+        }
+        return result;
+    },
+    p: 5566
+}
+
+export const getValue = helpers.getValue
+export const mapArray = helpers.mapArray
+export const p = helpers.p
